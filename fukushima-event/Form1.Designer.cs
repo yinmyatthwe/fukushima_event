4d06a3b1db13cadc3e07f087361be1d65045ae29
YinMyatThwe <yinmyatthwe1224@gmail.com>
2024-07-17 15:07:25 +0900

    7/17 updated（Gmapを使って有名なイベントがある市をレッドドットで表示する）

--- fukushima-event/Form1.Designer.cs
+++ fukushima-event/Form1.Designer.cs
@@ -30,11 +30,18 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.dateLabel = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.testData = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel4 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel5 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel6 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel7 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel8 = new System.Windows.Forms.LinkLabel();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -47,16 +54,6 @@
             this.label1.Size = new System.Drawing.Size(143, 46);
             this.label1.TabIndex = 0;
             this.label1.Text = "福島県";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(63, 93);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(83, 25);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "お知らせ";
             // 
             // label3
             // 
@@ -72,7 +69,7 @@
             // 
             this.dateLabel.AutoSize = true;
             this.dateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateLabel.Location = new System.Drawing.Point(614, 93);
+            this.dateLabel.Location = new System.Drawing.Point(579, 43);
             this.dateLabel.Name = "dateLabel";
             this.dateLabel.Size = new System.Drawing.Size(70, 25);
             this.dateLabel.TabIndex = 3;
@@ -81,31 +78,144 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(67, 125);
+            this.pictureBox1.Location = new System.Drawing.Point(428, 508);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(606, 236);
+            this.pictureBox1.Size = new System.Drawing.Size(338, 236);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
-            // testData
+            // linkLabel1
             // 
-            this.testData.AutoSize = true;
-            this.testData.Location = new System.Drawing.Point(386, 459);
-            this.testData.Name = "testData";
-            this.testData.Size = new System.Drawing.Size(51, 20);
-            this.testData.TabIndex = 5;
-            this.testData.Text = "label4";
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(64, 429);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(149, 20);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "うねめまつり （郡山市）";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(63, 467);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(468, 20);
+            this.linkLabel2.TabIndex = 7;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "相馬野馬追 （相馬市、南相馬市、飯舘村、浪江町、双葉町、大熊町）";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Location = new System.Drawing.Point(63, 508);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(164, 20);
+            this.linkLabel3.TabIndex = 8;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "麓山の火祭り （富岡町）";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // linkLabel4
+            // 
+            this.linkLabel4.AutoSize = true;
+            this.linkLabel4.Location = new System.Drawing.Point(63, 545);
+            this.linkLabel4.Name = "linkLabel4";
+            this.linkLabel4.Size = new System.Drawing.Size(190, 20);
+            this.linkLabel4.TabIndex = 9;
+            this.linkLabel4.TabStop = true;
+            this.linkLabel4.Text = "いわき七夕まつり （いわき市）";
+            this.linkLabel4.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel4_LinkClicked);
+            // 
+            // linkLabel5
+            // 
+            this.linkLabel5.AutoSize = true;
+            this.linkLabel5.Location = new System.Drawing.Point(63, 583);
+            this.linkLabel5.Name = "linkLabel5";
+            this.linkLabel5.Size = new System.Drawing.Size(181, 20);
+            this.linkLabel5.TabIndex = 10;
+            this.linkLabel5.TabStop = true;
+            this.linkLabel5.Text = "福島わらじまつり （福島市）";
+            this.linkLabel5.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel5_LinkClicked);
+            // 
+            // linkLabel6
+            // 
+            this.linkLabel6.AutoSize = true;
+            this.linkLabel6.Location = new System.Drawing.Point(63, 627);
+            this.linkLabel6.Name = "linkLabel6";
+            this.linkLabel6.Size = new System.Drawing.Size(176, 20);
+            this.linkLabel6.TabIndex = 11;
+            this.linkLabel6.TabStop = true;
+            this.linkLabel6.Text = "白河提灯まつり （白河市）";
+            this.linkLabel6.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel6_LinkClicked);
+            // 
+            // linkLabel7
+            // 
+            this.linkLabel7.AutoSize = true;
+            this.linkLabel7.Location = new System.Drawing.Point(64, 669);
+            this.linkLabel7.Name = "linkLabel7";
+            this.linkLabel7.Size = new System.Drawing.Size(176, 20);
+            this.linkLabel7.TabIndex = 12;
+            this.linkLabel7.TabStop = true;
+            this.linkLabel7.Text = "会津まつり （会津若松市）";
+            this.linkLabel7.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel7_LinkClicked);
+            // 
+            // linkLabel8
+            // 
+            this.linkLabel8.AutoSize = true;
+            this.linkLabel8.Location = new System.Drawing.Point(63, 708);
+            this.linkLabel8.Name = "linkLabel8";
+            this.linkLabel8.Size = new System.Drawing.Size(205, 20);
+            this.linkLabel8.TabIndex = 13;
+            this.linkLabel8.TabStop = true;
+            this.linkLabel8.Text = "会津田島祇園祭 （南会津町）";
+            this.linkLabel8.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel8_LinkClicked);
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemmory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(46, 71);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(720, 321);
+            this.gMapControl1.TabIndex = 15;
+            this.gMapControl1.Zoom = 0D;
+            this.gMapControl1.Load += new System.EventHandler(this.gMapControl1_Load);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 631);
-            this.Controls.Add(this.testData);
+            this.ClientSize = new System.Drawing.Size(808, 774);
+            this.Controls.Add(this.gMapControl1);
+            this.Controls.Add(this.linkLabel8);
+            this.Controls.Add(this.linkLabel7);
+            this.Controls.Add(this.linkLabel6);
+            this.Controls.Add(this.linkLabel5);
+            this.Controls.Add(this.linkLabel4);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.dateLabel);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -120,11 +230,18 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label dateLabel;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label testData;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.LinkLabel linkLabel4;
+        private System.Windows.Forms.LinkLabel linkLabel5;
+        private System.Windows.Forms.LinkLabel linkLabel6;
+        private System.Windows.Forms.LinkLabel linkLabel7;
+        private System.Windows.Forms.LinkLabel linkLabel8;
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
     }
 }
 
--- fukushima-event/Form1.cs
+++ fukushima-event/Form1.cs
@@ -8,6 +8,11 @@ using System.Text;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
 
 namespace fukushima_event
 {
@@ -16,6 +21,7 @@ namespace fukushima_event
         public Form1()
         {
             InitializeComponent();
+            InitializeMap();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -23,9 +29,49 @@ namespace fukushima_event
             //日付け
             dateLabel.Text = DateTime.Now.ToString("D");
             //testData.Text =;
-             Main();
+            Main();
+        }
+        //map
+        private void InitializeMap()
+        {
+            try
+            {
+                gMapControl1.MapProvider = GMapProviders.GoogleMap;
+                gMapControl1.Position = new PointLatLng(37.76072260, 140.4733561); // 福島県
+                gMapControl1.MinZoom = 2;
+                gMapControl1.MaxZoom = 18;
+                gMapControl1.Zoom = 10;
+                gMapControl1.AutoScroll = true;
+
+                // Add a marker
+                GMapMarker marker1 = new GMarkerGoogle(new PointLatLng(37.050419, 140.887680), GMarkerGoogleType.red_dot);　//いわき市
+                GMapMarker marker2 = new GMarkerGoogle(new PointLatLng(37.400529, 140.3597421), GMarkerGoogleType.red_dot);　//郡山市
+                GMapMarker marker3 = new GMarkerGoogle(new PointLatLng(37.7964381, 140.9194475), GMarkerGoogleType.red_dot); //相馬市
+                GMapMarker marker4 = new GMarkerGoogle(new PointLatLng(37.3439717, 140.96995645), GMarkerGoogleType.red_dot);//富岡町
+                GMapMarker marker5 = new GMarkerGoogle(new PointLatLng(37.8005851, 140.4003538), GMarkerGoogleType.red_dot); //福島市
+                GMapMarker marker6 = new GMarkerGoogle(new PointLatLng(37.1309437, 140.2560466), GMarkerGoogleType.red_dot);　//白河市
+                GMapMarker marker7 = new GMarkerGoogle(new PointLatLng(37.4520054, 139.9762868), GMarkerGoogleType.red_dot);　//会津市
+
+
+                GMapOverlay markers = new GMapOverlay("markers");
+                markers.Markers.Add(marker1);
+                markers.Markers.Add(marker2);
+                markers.Markers.Add(marker3);
+                markers.Markers.Add(marker4);
+                markers.Markers.Add(marker5);
+                markers.Markers.Add(marker6);
+                markers.Markers.Add(marker7);
+
+
+                gMapControl1.Overlays.Add(markers);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
 
         }
+
         //データー
         public static async Task Main()
         {
@@ -50,5 +96,48 @@ namespace fukushima_event
             }
         }
 
+
+        //fukushima matsuri link 
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("Chrome", "https://www.minpo.jp/pub/dento_gyoji2020/unemematuri");
+        }
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("Chrome", "https://www.minpo.jp/pub/dento_gyoji2020/soumanomaoi");
+        }
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("Chrome", "https://www.minpo.jp/pub/dento_gyoji2020/hayamanohimaturi");
+        }
+
+        private void linkLabel4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("Chrome", "https://www.minpo.jp/pub/dento_gyoji2020/iwakitanabatamaturi");
+        }
+
+        private void linkLabel5_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("Chrome", "https://www.minpo.jp/pub/dento_gyoji2020/fukushimawarajimaturi");
+        }
+
+        private void linkLabel6_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("Chrome", "https://www.minpo.jp/pub/dento_gyoji2020/shirakawatyoutinmaturi");
+        }
+        private void linkLabel7_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("Chrome", "https://www.minpo.jp/pub/dento_gyoji2020/aizumaturi");
+        }
+
+        private void linkLabel8_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("Chrome", "https://www.minpo.jp/pub/dento_gyoji2020/aizutajimagionsai");
+        }
+
+        private void gMapControl1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
